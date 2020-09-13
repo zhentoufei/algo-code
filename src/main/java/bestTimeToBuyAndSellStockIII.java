5d036114d4a1e50b1ee9c707d7bd791d651f2f75
xuling <xuling@meilin-inc.com>
2020-09-13 23:58:25 +0800

    带记忆字典的递归

--- src/main/java/bestTimeToBuyAndSellStockIII.java
+++ src/main/java/bestTimeToBuyAndSellStockIII.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class bestTimeToBuyAndSellStockIII {
 
@@ -9,8 +10,9 @@ public class bestTimeToBuyAndSellStockIII {
         if (null == prices || prices.length == 0) {
             return maxProfit;
         }
-        maxProfit = dfsProfit(prices, 0, 0, 0);
-
+        // maxProfit = dfsProfit(prices, 0, 0, 0);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        maxProfit = dfsProfitWithDict(prices, 0, 0, 0, map);
         return maxProfit;
     }
     // 这个方法超时了
@@ -34,6 +36,29 @@ public class bestTimeToBuyAndSellStockIII {
         }
 
         return Math.max(keepIncome, Math.max(buyIncome, saleIncome));
+    }
+
+    public static Integer dfsProfitWithDict(int[] prices, int index, int status, int buyTime, Map<String, Integer> res){
+        String key = String.valueOf(index) + "_" + String.valueOf(status) + "_" + String.valueOf(buyTime);
+        if(res.containsKey(key)){
+            return res.get(key);
+        }
+        if(index == prices.length || buyTime == 2){
+            res.put(key, 0);
+            return 0;
+        }
+        int keepIncome;
+        int buyIncome = 0;
+        int saleIncome = 0;
+
+        keepIncome = dfsProfitWithDict(prices, index + 1, status, buyTime, res);
+        if(status == 1){
+            saleIncome = dfsProfitWithDict(prices, index + 1, 0, buyTime + 1, res) + prices[index];
+        }else{
+            buyIncome = dfsProfitWithDict(prices, index + 1, 1, buyTime, res) - prices[index];
+        }
+        res.put(key, Math.max(keepIncome, Math.max(buyIncome, saleIncome)));
+        return res.get(key);
 
     }
 
